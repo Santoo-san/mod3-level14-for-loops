dc127ea5c8e14af0e426188efc440b7065448b93
Santoo-san <67684816+Santoo-san@users.noreply.github.com>
2020-09-07 15:46:03 +0200

    Add files via upload

---
+++ even-odd-reporter.js
@@ -0,0 +1,8 @@
+console.log("even odd reporter");
+
+for (i = 1; i <= 20; i++) {
+  if (i % 2 == 0) console.log(i + " even");
+  else console.log(i + " odd");
+}
+
+console.log("reached 20");
---
+++ multiplication-tables.js
@@ -0,0 +1,17 @@
+console.log("multiplication tables");
+
+//tafel 9
+const number = 9;
+
+for (i = 0; i <= 10; i++) {
+  console.log(i * number);
+}
+
+//tafels t/m 100
+console.log("one to ten");
+
+for (i = 1; i <= 10; i++) {
+  for (j = 1; j <= 10; j++) {
+    console.log(i * j);
+  }
+}
